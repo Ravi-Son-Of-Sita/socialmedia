fcf58beb5ea445dbf094595287363e846cf626af
Ravi-Son-Of-Sita <rkyadav.1991.1991@gmail.com>
2022-12-08 21:45:49 +0530

    initial commit

--- src/component/LeftBar.jsx
+++ src/component/LeftBar.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import "./leftbar.scss"
+import ProfilePic from './ProfilePic'
+import {FaUserFriends,FaClock} from "react-icons/fa"
+import {TiGroup} from "react-icons/ti"
+import {BsFillCaretRightSquareFill} from "react-icons/bs"
+import {AiFillDownCircle} from "react-icons/ai"
 
 function LeftBar() {
   return (
     <div className="left-bar">
         <div className="top-part">
+          <div>
+          <ProfilePic />
+          <span>Ravi Ranjan Kumar</span>
+          </div>
+          <div>
+            <div><FaUserFriends size={'1.75em'}/></div>
+            <span>Friends</span>
+          </div>
+          <div>
+            <div><FaClock size={'1.5em'}/></div>
+            <span>Most Recent</span>
+          </div>
+          <div>
+            <div><TiGroup size={'1.75em'}/></div>
+            <span>Groups</span>
+          </div>
+          <div>
+            <div><BsFillCaretRightSquareFill size={'1.5em'}/></div>
+            <span>Watch</span>
+          </div>
+          <div>
+            <div><AiFillDownCircle size={'1.75em'}/></div>
+            <span>More</span>
+          </div>
+
+
         </div>
+        <hr/>
         <div className="middle-part">middle</div>
+        <hr/>
         <div className="bottom-part">bottom</div>
     </div>
     
--- src/component/RightBar.jsx
+++ src/component/RightBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./rightbar.scss"
 
 function RightBar() {
   return (
