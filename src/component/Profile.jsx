152de45f3bd93a33ceaac10954de304d953fcb83
Ravi-Son-Of-Sita <rkyadav.1991.1991@gmail.com>
2022-12-12 07:53:02 +0530

    initial commit

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { useContext } from 'react';
+import{FiEdit} from 'react-icons/fi'
+import {BiCamera} from 'react-icons/bi'
+import { AuthContext } from '../auth/authContext';
+import ProfilePic from "./ProfilePic"
 function Profile() {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div>profile</div>
+    <div >
+      <div>
+        <div>
+          <ProfilePic/>
+          <BiCamera/>
+        </div>
+        <span>Ravi Ranjan Kumar</span>
+        <FiEdit Style={currentUser?{display:'flex'}:{display:'none'}}/>
+      </div>
+
+    </div>
   )
 }
 
